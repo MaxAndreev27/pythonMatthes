fb1303943f26d9c6e44730b7cd7aeea656bc6b55
max <maxandreev27@gmail.com>
2026-04-04 20:20:20 +0300

    Alien invasion game: ship move and bullets

---
+++ alien_invasion/settings.py
@@ -0,0 +1,16 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 3
+        # Параметры снаряда
+        self.bullet_speed = 6.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 6
