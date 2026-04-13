dbb9a9b42d7b890e810f2a1f4799c32f5ab84b9a
max <maxandreev27@gmail.com>
2026-04-13 18:42:45 +0300

    Add django-bootstrap5==26.2

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     # User apps
     "learning_logs",
     "accounts",
+    # Third party apps.
+    "django_bootstrap5",
     # Default apps
     "django.contrib.admin",
     "django.contrib.auth",
